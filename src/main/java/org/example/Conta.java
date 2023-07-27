70542e151389d1433fa85413c373c74bc6b26f48
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-26 22:24:35 -0300

    Conta.java Implemented.

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -3,50 +3,53 @@ package org.example;
 import java.math.BigDecimal;
 
 public class Conta {
-
-    private String nome;
+    private String nomeCompleto;
     private BigDecimal saldo;
+    private BigDecimal saldoInvestimento;
 
-    private BigDecimal saldoInvestido;
-
-    public Conta() {
-        this.saldo = BigDecimal.valueOf(100); // Instanciar com 100 unidades
+    public Conta(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+        this.saldo = new BigDecimal("100.0"); // Saldo inicial com 100 unidades
+        this.saldoInvestimento = BigDecimal.ZERO; // Saldo de investimento inicialmente 0
     }
 
+    // Método para resgatar um valor da conta
     public void resgate(BigDecimal valor) {
-        saldo = saldo.subtract(valor);
-        System.out.println(saldo);
+        if (valor.compareTo(saldo) <= 0) {
+            saldo = saldo.subtract(valor);
+            System.out.println("Resgate de " + valor + " unidades realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para o resgate.");
+        }
     }
 
+    // Método para realizar um depósito na conta
     public void deposito(BigDecimal valor) {
         saldo = saldo.add(valor);
+        System.out.println("Depósito de " + valor + " unidades realizado com sucesso.");
     }
 
-    public BigDecimal consultarSaldo() {
-        return saldo;
+    // Método para visualizar os dados da conta e saldo
+    public void visualizarDadosConta() {
+        System.out.println("Nome completo: " + nomeCompleto);
+        System.out.println("Saldo disponível: " + saldo + " unidades");
+        System.out.println("Saldo de investimento: " + saldoInvestimento + " unidades");
     }
 
-    public String getNome() {
-        return nome;
+    // Método para realizar um investimento
+    public void investimento(BigDecimal valor) {
+        if (valor.compareTo(saldo) <= 0) {
+            saldo = saldo.subtract(valor);
+            saldoInvestimento = saldoInvestimento.add(valor);
+            System.out.println("Investimento de " + valor + " unidades realizado com sucesso.");
+        } else {
+            System.out.println("Saldo insuficiente para o investimento.");
+        }
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    // Método para calcular o lucro do investimento
+    private BigDecimal calcularLucroInvestimentoCiclo() {
+        return saldoInvestimento.multiply(BigDecimal.valueOf(0.05)) ; // 5% de lucro no valor investido
     }
 
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(BigDecimal saldo) {
-        this.saldo = saldo;
-    }
-
-    public BigDecimal getSaldoInvestido() {
-        return saldoInvestido;
-    }
-
-    public void setSaldoInvestido(BigDecimal saldoInvestido) {
-        this.saldoInvestido = saldoInvestido;
-    }
 }
