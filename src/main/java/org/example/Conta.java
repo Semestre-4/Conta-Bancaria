5b9b167bb647f57034e699a7d92eb1d60a7980bb
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-26 21:12:19 -0300

    first commit

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -1,4 +1,21 @@
 package org.example;
 
 public class Conta {
+
+    private String nome;
+
+    private void resgate() {}
+
+    private void deposito(){}
+
+    private void consultarSaldo(){}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
 }
