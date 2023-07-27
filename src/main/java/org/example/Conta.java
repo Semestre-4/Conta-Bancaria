3f7d22260ea73f6a09d0858b1fe30048663801c5
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-26 21:45:19 -0300

    saldo investido added

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -7,8 +7,9 @@ public class Conta {
     private String nome;
     private BigDecimal saldo;
 
+    private BigDecimal saldoInvestido;
+
     public Conta() {
-        this.nome = nome;
         this.saldo = BigDecimal.valueOf(100); // Instanciar com 100 unidades
     }
 
@@ -38,5 +39,13 @@ public class Conta {
 
     public void setSaldo(BigDecimal saldo) {
         this.saldo = saldo;
+    }
+
+    public BigDecimal getSaldoInvestido() {
+        return saldoInvestido;
+    }
+
+    public void setSaldoInvestido(BigDecimal saldoInvestido) {
+        this.saldoInvestido = saldoInvestido;
     }
 }
