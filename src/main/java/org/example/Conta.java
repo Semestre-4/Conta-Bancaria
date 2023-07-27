7df1d4873c09ce967e7c613e5b1e9f74e6f73cd4
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-26 21:23:23 -0300

    Optimizations

--- src/main/java/org/example/Conta.java
+++ src/main/java/org/example/Conta.java
@@ -5,35 +5,22 @@ import java.math.BigDecimal;
 public class Conta {
 
     private String nome;
-
     private BigDecimal saldo;
 
-    private void resgate(BigDecimal valor) {
+    public Conta(String nome) {
+        this.nome = nome;
+        this.saldo = BigDecimal.valueOf(100); // Instanciar com 100 unidades
+    }
+
+    public void resgate(BigDecimal valor) {
         saldo = saldo.subtract(valor);
     }
 
-    private void deposito(BigDecimal valor) {
+    public void deposito(BigDecimal valor) {
         saldo = saldo.add(valor);
     }
 
-    private BigDecimal consultarSaldo() {
+    public BigDecimal consultarSaldo() {
         return saldo;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public BigDecimal getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(BigDecimal saldo) {
-        this.saldo = BigDecimal.valueOf(100);
     }
 }
